c4c7235df3394b85d5593f873f97972c50edd11f
Julian Grimm <grimmjulian225@gmail.com>
2026-09-03 16:15:55 +0200

    Add a basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "regexcite - Make Regular Expressions More Exciting";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        r-packages = with pkgs.rPackages; [
+          stringr
+          testthat
+          roxygen2
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+          (pkgs.rWrapper.override {
+            packages = r-packages;
+          })
+          ];
+          shellHook = ''
+            echo "regexcite development environment loaded"
+            echo "R version: $(R --version | head -1)"
+          '';
+        };
+      }
+    );
+}
